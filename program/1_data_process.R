301476fbbd4f67e02091f6563a9bbc0fa81d01ac
Wei Guo <weiguo@berkeley.edu>
2025-09-28 05:03:03 +0200

    Add files via upload

--- program/1_data_process.R
+++ program/1_data_process.R
@@ -35,34 +35,34 @@ output_folder <<- paste0(path,"/Data/Output")
 program_folder <<- paste0(path,"/Program/R")
 
 ######
-# Load and initialize the data made by Meri
+# Load and initialize the data made by corelogic
 
 setwd(data_folder)
-meri_data <- read_dta("property_sales_2010_2022.dta") %>%
+corelogic_data <- read_dta("property_sales_2010_2022.dta") %>%
   as.data.table()
 
 # select only useful variables
-meri_data <- meri_data %>%
+corelogic_data <- corelogic_data %>%
   select(clip:blockgroup,buildinggrosssquarefeet_r,parcellevellongitude,
          parcellevellatitude,buyer1fullname:buyer1firstnameandmiddleinitial) 
 
 # trim the data to remove outliers
-meri_data <- meri_data[ !is.na(saleamount_r) ]
-meri_data <- meri_data[ saleamount_r <= quantile(meri_data$saleamount_r, 0.99, na.rm= T) ]
-meri_data <- meri_data[ saleamount_r >= quantile(meri_data$saleamount_r, 0.01, na.rm= T) ]
-meri_data <- meri_data[ buildinggrosssquarefeet_r <= quantile(meri_data$buildinggrosssquarefeet_r, 0.99, na.rm= T) ]
-meri_data <- meri_data[ acres_r  <= quantile(meri_data$acres_r , 0.99, na.rm= T) ]
-meri_data <- meri_data[ stories <= quantile(meri_data$stories, 0.99, na.rm= T) ]
-meri_data <- meri_data[ bathrooms <= quantile(meri_data$bathrooms, 0.99, na.rm= T) ]
-meri_data <- meri_data[ rooms <= quantile(meri_data$rooms, 0.99, na.rm= T) ]
-write_dta(meri_data, "")
+corelogic_data <- corelogic_data[ !is.na(saleamount_r) ]
+corelogic_data <- corelogic_data[ saleamount_r <= quantile(corelogic_data$saleamount_r, 0.99, na.rm= T) ]
+corelogic_data <- corelogic_data[ saleamount_r >= quantile(corelogic_data$saleamount_r, 0.01, na.rm= T) ]
+corelogic_data <- corelogic_data[ buildinggrosssquarefeet_r <= quantile(corelogic_data$buildinggrosssquarefeet_r, 0.99, na.rm= T) ]
+corelogic_data <- corelogic_data[ acres_r  <= quantile(corelogic_data$acres_r , 0.99, na.rm= T) ]
+corelogic_data <- corelogic_data[ stories <= quantile(corelogic_data$stories, 0.99, na.rm= T) ]
+corelogic_data <- corelogic_data[ bathrooms <= quantile(corelogic_data$bathrooms, 0.99, na.rm= T) ]
+corelogic_data <- corelogic_data[ rooms <= quantile(corelogic_data$rooms, 0.99, na.rm= T) ]
+write_dta(corelogic_data, "")
 
 # add race info
-meri_data[,id := 1:.N]
-prop_name <- meri_data[,c("id","buyer1lastname")]
+corelogic_data[,id := 1:.N]
+prop_name <- corelogic_data[,c("id","buyer1lastname")]
 prop_name[grepl("EQUITIES|LLC|TRUSTEE|HOLDINGS|MANAGERS|REALTY|
                 ASSOCIATES|STREET|OWNERS|PROPERTY|CORP|ASSOCI|INC|
-                LIMITED|AMERICAN|FAMILY|CHURCH|VILLAGE|TRUST|
+                LIMITED|AcorelogicCAN|FAMILY|CHURCH|VILLAGE|TRUST|
                 PLACE|OF|AVENUE|HOUSING|RECREATION|PARKS|associates|
                 properties|owner|city|dept|texas|harris|galveston|HOLDING|agent|real|owners|
                 management|preservation|department|gregory|estate|property|
@@ -80,37 +80,37 @@ prop_race <- predict_race(voter.file = prop_name,
 prop_race <- prop_race %>% 
   select(id, surname:pred.oth)
 
-meri_data <- merge(meri_data, prop_race, by = "id", all.x = T) 
+corelogic_data <- merge(corelogic_data, prop_race, by = "id", all.x = T) 
 # remove columns without race info
-meri_data <- meri_data %>% subset(!is.na(pred.whi))
+corelogic_data <- corelogic_data %>% subset(!is.na(pred.whi))
 
-# generate summary statistics for Meri's data, repectively
-meri_summ <- datasummary( year+month+saleamount_r+fireplace+pool+AC+heating_central+
+# generate summary statistics for corelogic's data, repectively
+corelogic_summ <- datasummary( year+month+saleamount_r+fireplace+pool+AC+heating_central+
                             foundation_slab+garage+age+bathrooms+rooms+acres_r+buildinggrosssquarefeet_r+stories+building_quality+
                             building_traditional+homestead+mobile_home+
                             sfha+hazard+harvey_inundation+ike_inunda+
                             pred.whi+pred.bla+pred.his+pred.asi+pred.oth
                           ~N+Mean+SD+Min+P25+P50+P75+Max,
-                          data = meri_data,
+                          data = corelogic_data,
                           fmt = '%.2f',
-                          output = "meri_summ.csv",
-                          title = 'Summary Statistics for Property Transactions (Meri)') 
+                          output = "corelogic_summ.csv",
+                          title = 'Summary Statistics for Property Transactions (corelogic)') 
 
 # generate unique neighborhood identifier 
 # census_tract + year + luxuriousness (building sqft > 2000) + sfha
-meri_data[, luxury := buildinggrosssquarefeet_r >= 2400]
-meri_data[, neighborhood := paste0(censustract,"_",year,"_",luxury,"_",sfha)]
-meri_data[, neigh_id := as.numeric(as.factor(neighborhood))]
-meri_data <- meri_data %>% arrange(neigh_id)
+corelogic_data[, luxury := buildinggrosssquarefeet_r >= 2400]
+corelogic_data[, neighborhood := paste0(censustract,"_",year,"_",luxury,"_",sfha)]
+corelogic_data[, neigh_id := as.nucorelogicc(as.factor(neighborhood))]
+corelogic_data <- corelogic_data %>% arrange(neigh_id)
 
 # save neighborhood matrix
-meri_neigh <- meri_data[, c("neigh_id","neighborhood","censustract","year","luxury","sfha")] %>%
+corelogic_neigh <- corelogic_data[, c("neigh_id","neighborhood","censustract","year","luxury","sfha")] %>%
   arrange(neigh_id) %>%
   unique()
-write_fst(meri_neigh, "final_sample_2010_2022.fst")
+write_fst(corelogic_neigh, "final_sample_2010_2022.fst")
 
 # save the choice matrix
-write_fst(meri_data, "choice_tract_year_2010_2022.fst")
+write_fst(corelogic_data, "choice_tract_year_2010_2022.fst")
 
 
 
